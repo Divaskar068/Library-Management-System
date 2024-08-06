20c0b548fb57c69c7d9f0a4b456e989bc1b3895d
Divaskar <130750966+Divaskar068@users.noreply.github.com>
2024-08-06 11:12:07 +0400

    first commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+public void main(String args[]) {
+    /* Set the Nimbus look and feel */
+    try {
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                break;
+            }
+        }
+    } catch (Exception ex) {
+        java.util.logging.Logger.getLogger(home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
+
+    /* Create and display the form */
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new home().setVisible(true);
+        }
+    });
+}
